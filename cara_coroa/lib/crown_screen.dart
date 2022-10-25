988534309c730b0d194f7a194d59efc51c607663
borgonha <amiltonborgonha.exis@gmail.com>
2022-10-24 22:21:02 -0300

    calculos randomicos

--- cara_coroa/lib/crown_screen.dart
+++ cara_coroa/lib/crown_screen.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class CrownScreen extends StatefulWidget {
-  bool? crown;
+  String? crown;
+
+  CrownScreen({this.crown});
+
   @override
   State<CrownScreen> createState() => _CrownScreenState();
 }
@@ -10,6 +12,43 @@ class CrownScreen extends StatefulWidget {
 class _CrownScreenState extends State<CrownScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var caminhoImagem;
+    if (widget.crown == 'cara') {
+      caminhoImagem = 'assets/images/moeda_cara.png';
+    } else {
+      caminhoImagem = 'assets/images/moeda_coroa.png';
+    }
+    return Scaffold(
+      backgroundColor: const Color(0xff61bd86),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: Image(
+                    image: AssetImage(caminhoImagem),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: const Image(
+                      image: AssetImage('assets/images/botao_voltar.png'),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ]),
+    );
   }
 }
--- cara_coroa/lib/main_screen.dart
+++ cara_coroa/lib/main_screen.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:math';
+
+import 'package:cara_coroa/crown_screen.dart';
+import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -7,8 +9,57 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  void _sorteio() {
+    var number = Random().nextInt(200);
+    String? resultado;
+
+    if ((number % 2) == 0) {
+      resultado = 'cara';
+    } else {
+      resultado = 'coroa';
+    }
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CrownScreen(crown: resultado),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xff61bd86),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 50),
+                  child: Image(
+                    image: AssetImage('assets/images/logo.png'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: GestureDetector(
+                    onTap: _sorteio,
+                    child: const Image(
+                      image: AssetImage('assets/images/botao_jogar.png'),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ]),
+    );
   }
 }
