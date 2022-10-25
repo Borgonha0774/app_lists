52c0a0e274eaa09007a132bc22202f11e21ae5ca
borgonha <amiltonborgonha.exis@gmail.com>
2022-10-25 20:22:08 -0300

    listagem

--- lists_flutter/lib/main.dart
+++ lists_flutter/lib/main.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:lists_flutter/home.dart';
 
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
